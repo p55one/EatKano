72003e206ec7aef920c1c4f38794dadd4c22f8d6
p55one <98623901+p55one@users.noreply.github.com>
2022-01-29 07:39:22 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -441,11 +441,11 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
             SubmitResults();
         }
 
-        if (cps <= 5) return '试着好好练一下？';
-        if (cps <= 8) return 'TCL';
-        if (cps <= 10)  return 'TQL';
-        if (cps <= 15) return '您';
-        return '人？';
+        if (cps <= 5) return '铸币吧';
+        if (cps <= 8) return '。。。。。';
+        if (cps <= 10)  return 'Q都能Q歪来';
+        if (cps <= 15) return '太牛辣';
+        return '飞行棋职业选手';
     }
 
     function toStr(obj) {
